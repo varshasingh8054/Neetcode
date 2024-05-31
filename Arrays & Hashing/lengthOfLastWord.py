baba027a78b76384539f61161527d6f013ff6379
Varsha Singh <varshasingh8054>
2024-06-01 05:08:54 +0530

    lengthOfLastWord

---
+++ Arrays & Hashing/lengthOfLastWord.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+        count = 0
+        while s[i] == ' ':
+            i -= 1
+        while i>=0 and s[i] != " " :
+            count = count + 1
+            i -= 1
+
+        return count
+
+solution = Solution()
+print(solution.lengthOfLastWord("   fly me   to   the moon  "))
